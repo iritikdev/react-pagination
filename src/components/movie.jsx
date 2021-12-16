3c33a2713f0d72ab5fb48f58c61014766d8af737
Ritik Sharma <54641006+iritikdev@users.noreply.github.com>
2021-12-16 15:46:51 +0530

    Add Genre component

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { getMovies } from "../service/fakeMovieService";
 import Like from "./Like";
 import Pagination from "./pagination";
-import { paginate } from "./utils/paginate";
+import { paginate } from "../utils/paginate";
+import Genre from "./genre";
 
 class Movies extends Component {
   state = {
@@ -38,46 +39,53 @@ class Movies extends Component {
 
     return (
       <>
-        <p className="alert alert-primary mt-3 p-2" role="alert">
-          {count === 0
-            ? "There is no movies in database."
-            : `Showing ${count} movies in the database.`}
-        </p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <th>{movie.title}</th>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}
-                  />
-                </td>
-                <td onClick={() => this.handleDelete(movie)}>
-                  <ion-icon name="trash-outline"></ion-icon>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          onPageChange={this.handlePageChange}
-          currentPage={currentPage}
-        />
+        <div className="row">
+          <div className="col-3">
+            <Genre />
+          </div>
+          <div className="col">
+            <p className="alert alert-primary mt-3 p-2" role="alert">
+              {count === 0
+                ? "There is no movies in database."
+                : `Showing ${count} movies in the database.`}
+            </p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Stock</th>
+                  <th scope="col">Rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((movie) => (
+                  <tr key={movie._id}>
+                    <th>{movie.title}</th>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Like
+                        liked={movie.liked}
+                        onClick={() => this.handleLike(movie)}
+                      />
+                    </td>
+                    <td onClick={() => this.handleDelete(movie)}>
+                      <ion-icon name="trash-outline"></ion-icon>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              onPageChange={this.handlePageChange}
+              currentPage={currentPage}
+            />
+          </div>
+        </div>
       </>
     );
   }
